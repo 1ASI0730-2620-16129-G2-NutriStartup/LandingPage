88bc88f2fc38fe348eb26fb101c931c1f32a2daa
ALBA <146106378+GAALBA02@users.noreply.github.com>
2026-09-23 20:28:30 -0500

    feat: add progreso page components

---
+++ objetivo.js
@@ -0,0 +1,52 @@
+// ======================================
+// OBJETIVOS - VitaControl
+// ======================================
+
+const formulario = document.querySelector(".goal-form");
+
+formulario.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    // Obtener datos
+
+    const pesoActual = document.querySelector('input[placeholder="Ejemplo: 75"]').value;
+
+    const pesoObjetivo = document.querySelector('input[placeholder="Ejemplo: 68"]').value;
+
+    const objetivo = document.querySelectorAll("select")[0].value;
+
+    const tiempo = document.querySelectorAll("select")[1].value;
+
+    const actividad = document.querySelectorAll("select")[2].value;
+
+
+    // Validar
+
+    if(pesoActual==="" || pesoObjetivo===""){
+
+        alert("⚠️ Complete todos los campos.");
+
+        return;
+
+    }
+
+
+    // Guardar en LocalStorage
+
+    localStorage.setItem("pesoActual", pesoActual);
+
+    localStorage.setItem("pesoObjetivo", pesoObjetivo);
+
+    localStorage.setItem("objetivo", objetivo);
+
+    localStorage.setItem("tiempo", tiempo);
+
+    localStorage.setItem("actividad", actividad);
+
+
+    // Mensaje
+
+    alert("✅ Objetivos guardados correctamente.");
+
+});
